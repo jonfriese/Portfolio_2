9be6f57ac4d955cc0589b508d22e509980348014
noj33 <jonfriese33@gmail.com>
2013-10-15 23:57:44 -0700

    nearly completed authentication tests

--- test/features/posts/post_create_test.rb
+++ test/features/posts/post_create_test.rb
@@ -3,6 +3,12 @@ require "test_helper"
 feature "creating a post" do
   scenario "with valid data" do
 
+    visit new_user_session_path
+    fill_in "Email", with: users(:fake).email
+    fill_in "Password", with: "password"
+
+    click_on "Sign in"
+
     # Given a completed post form
     visit posts_path
     click_on "New Post"
@@ -18,6 +24,8 @@ feature "creating a post" do
 
     # Add a success message
     page.text.must_include 'Post was successfully created.'
+    page.has_css? "#author"
+    page.text.must_include users(:fake).email
 
   end
 end
--- test/features/posts/post_edit_test.rb
+++ test/features/posts/post_edit_test.rb
@@ -2,10 +2,18 @@ require "test_helper"
 
 feature "editing a post" do
   scenario "a post is present" do
+
+    visit new_user_session_path
+    fill_in "Email", with: users(:fake).email
+    fill_in "Password", with: "password"
+    click_on "Sign in"
+
     post = Post.create(title: "Becoming a Code Fellow", body: "Means striving for excellence.")
     visit post_path(post)
+
     click_on "Edit"
     fill_in 'Body', with: 'this post is new'
+
     click_on "Update Post"
     page.text.must_include 'this post is new'
   end
