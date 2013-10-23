3ed391d288517d72d6533a1cc73fed533d7575da
noj33 <jonfriese33@gmail.com>
2013-10-22 18:20:58 -0700

    comment test

--- test/features/posts/create_comment_test.rb
+++ test/features/posts/create_comment_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 feature "Commenting on a post" do
-  scenario "the user is allowed to comment" do
-    visit root_path
+  scenario "any user is allowed to comment" do
+
+    visit post_path(posts(:cr))
+
+    fill_in :comment_author, with:       "Bill"
+    fill_in :comment_author_email, with: "Bill@example.com"
+    fill_in :comment_content, with:      "Dis posts sucks"
+
+    click_on "Submit for approval"
+
+    page.text.must_include "Comment is awaiting moderation"
+
   end
 end
