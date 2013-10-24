2f6603e1b85ad655d52d806603f25ccfb620b021
noj33 <jonfriese33@gmail.com>
2013-10-24 16:55:09 -0700

    heroku debugging

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
-  attr_accessible :approved, :author, :author_email, :author_url,
-  :content, :referrer, :user_agent, :user_ip, :post_id, :commentable_id, :commentable_type
+  attr_accessible :approved, :author, :author_email, :author_url, :content, :referrer,
+                  :user_agent, :user_ip, :post_id, :commentable_id, :commentable_type
 end
