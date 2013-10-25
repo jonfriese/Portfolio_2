b066487716290f865fe177a40acfd85d61a1c9c7
noj33 <jonfriese33@gmail.com>
2013-10-24 18:58:04 -0700

    heroku debug

--- db/migrate/20131024020427_add_time_and_date_to_projects.rb
+++
@@ -1,6 +0,0 @@
-class AddTimeAndDateToProjects < ActiveRecord::Migration
-  def change
-    add_column :projects, :created_at, :datetime
-    add_column :projects, :updated_at, :datetime
-  end
-end
